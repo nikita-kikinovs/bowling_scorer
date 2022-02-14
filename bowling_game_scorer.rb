e21c58389825e03cae6117b6375d406c485df678
Ņikita Kikiņovs <nikita.kikinovs@catchsmart.com>
2022-02-14 19:16:22 +0200

    Implement input processing

--- bowling_game_scorer.rb
+++ bowling_game_scorer.rb
@@ -1,5 +1,5 @@
 class BowlingGameScorer
-  SCORE_INPUT_REGEX = //
+  SCORE_INPUT_REGEX = /\A((10|[0-9]),){10,20}(10|[0-9])\z/
 
   attr_accessor :input
 
@@ -14,7 +14,76 @@ class BowlingGameScorer
   private
 
   def process_input
+    print_invalid_input_message unless input.match(SCORE_INPUT_REGEX)
+
+    splitted = input.split(',').map(&:to_i)
+    processed = []
+    skip_next = false
+
+    splitted.each_with_index do |throw, i|
+      if skip_next
+        skip_next = false
+        next
+      end
+
+      if processed.size == 9
+        processed << splitted[i..]
+        break
+      end
+
+      case throw
+        when 10
+          processed << [throw]
+        when 0..9
+          processed << [throw, splitted[i + 1]]
+          skip_next = true
+      end
+    end
+
+    @input = processed
+
+    print_invalid_frame_message unless valid_frames?
+    print_invalid_last_frame_message unless valid_last_frame?
+  end
+
+  def valid_frames?
+    input.count == 10 && input[0..8].all? { |frame| frame.sum <= 10 }
+  end
+
+  def valid_last_frame?
+    frame = input.last
+    valid_last_frame_two_throws?(frame) || valid_last_frame_three_throws?(frame)
+  end
+
+  def valid_last_frame_two_throws?(frame)
+    frame.size == 2 && frame.sum < 10
+  end
+
+  def valid_last_frame_three_throws?(frame)
+    frame.size == 3 && (strike?([frame.first]) || spare?(frame.first(2)))
+  end
+
+  def strike?(frame)
+    frame.first == 10 && frame.size == 1
+  end
+
+  def spare?(frame)
+    !strike?(frame) && frame.first(2).sum == 10
+  end
+
+  def print_invalid_input_message
+    abort 'Invalid input! First argument should be a numbers of pins ' \
+          'knocked down by each ball separated by commas without spaces!'
+  end
+
+  def print_invalid_last_frame_message
+    abort 'Invalid last (10th) frame!'
+  end
+
+  def print_invalid_frame_message
+    abort 'Invalid frame count or frame scores!'
   end
 end
 
+# Comment line below when running tests
 BowlingGameScorer.new(ARGV[0]).calculate_score
--- bowling_game_scorer_spec.rb
+++ bowling_game_scorer_spec.rb
@@ -1,4 +1,96 @@
 require './bowling_game_scorer'
 
 describe BowlingGameScorer do
+  describe '#initialize' do
+    subject { BowlingGameScorer.new(score) }
+    let(:score) { input_score }
+
+    context 'with invalid input data' do
+      context 'with invalid frame count' do
+        let(:err_msg) { 'Invalid input! First argument should be a numbers of pins ' \
+                         "knocked down by each ball separated by commas without spaces!\n" }
+
+        context 'bigger than 10' do
+          let(:input_score) { '3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2' }
+
+          specify { expect { to_fail }.to output(err_msg).to_stderr }
+        end
+
+        context 'smaller than 10' do
+          let(:input_score) { '10,10,10,10,10,10,10,10,10' }
+
+          specify { expect { to_fail }.to output(err_msg).to_stderr }
+        end
+      end
+
+      context 'when frame score is bigger than 10' do
+        let(:input_score) { '3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,9,2,3,2' }
+        let(:err_msg) { "Invalid frame count or frame scores!\n" }
+
+        specify { expect { to_fail }.to output(err_msg).to_stderr }
+      end
+
+      context 'when last frame first throw is strike' do
+        let(:err_msg) { "Invalid last (10th) frame!\n" }
+
+        context 'when only 1 additional throw is done' do
+          let(:input_score) { '3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,10,1' }
+
+          specify { expect { to_fail }.to output(err_msg).to_stderr }
+        end
+
+        context 'when no additional throws are done' do
+          let(:input_score) { '3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,10' }
+
+          specify { expect { to_fail }.to output(err_msg).to_stderr }
+        end
+      end
+
+      context 'when last frame is spare and no additional throws are done' do
+        let(:input_score) { '3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,2,8' }
+        let(:err_msg) { "Invalid last (10th) frame!\n" }
+
+        specify { expect { to_fail }.to output(err_msg).to_stderr }
+      end
+
+      context 'when last frame is open and third throw is done' do
+        let(:input_score) { '3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,2,2,2' }
+        let(:err_msg) { "Invalid last (10th) frame!\n" }
+
+        specify { expect { to_fail }.to output(err_msg).to_stderr }
+      end
+    end
+
+    context 'with valid input data' do
+      context 'with last open frame' do
+        let(:input_score) { '3,2,10,3,2,3,2,10,3,2,3,2,3,7,3,2,2,3' }
+
+        it 'should correctly process score' do
+          expect(subject.input).to eq([[3, 2], [10], [3, 2], [3, 2], [10], [3, 2], [3, 2], [3, 7], [3, 2], [2, 3]])
+        end
+      end
+
+      context 'with last spare' do
+        let(:input_score) { '3,2,10,3,2,3,2,10,3,2,3,2,3,7,3,2,2,8,5' }
+
+        it 'should correctly process score' do
+          expect(subject.input).to eq([[3, 2], [10], [3, 2], [3, 2], [10], [3, 2], [3, 2], [3, 7], [3, 2], [2, 8, 5]])
+        end
+      end
+
+      context 'with last strike' do
+        let(:input_score) { '3,2,10,3,2,3,2,10,3,2,3,2,3,7,3,2,10,6,10' }
+
+        it 'should correctly process score' do
+          expect(subject.input).to eq([[3, 2], [10], [3, 2], [3, 2], [10], [3, 2], [3, 2], [3, 7], [3, 2], [10, 6, 10]])
+        end
+      end
+    end
+  end
+end
+
+def to_fail
+  expect { subject }.to raise_error(SystemExit) do |error|
+    expect(error.status).to eq(1)
+  end
 end
